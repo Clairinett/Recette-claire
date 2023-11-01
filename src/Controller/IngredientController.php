4afd998f64bc255e2d2c80702baf01caae4294fe
[Clairinette] <[claire.crinier@gmail.com]>
2023-11-01 19:45:26 +0100

    ajout d'un formulaire pour ajouter un ingrédient

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ingredient;
+use App\Form\IngredientType;
+use Doctrine\ORM\EntityManager;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +15,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'ingredient')]
+    #[Route('/ingredient', name: 'ingredient.index', methods: ['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         /*Cette fonction récupère tout les ingrédients */
@@ -24,6 +28,25 @@ class IngredientController extends AbstractController
 
         return $this->render('pages/ingredient/index.html.twig', [
             'ingredients' => $ingredients
+        ]);
+    }
+
+    #[Route('/nouveau', name: 'ingredient.new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager) : Response
+    {
+        $ingredient = new Ingredient();
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+            /*dd($ingredient);*/
+            $manager->persist($ingredient);
+            $manager->flush();
+        }
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
