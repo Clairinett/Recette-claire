beb4b3e0eeb2711746fbba1c373ea7105c0a63cd
[Clairinette] <[claire.crinier@gmail.com]>
2023-11-01 17:07:46 +0100

    ajout de la page ingrédient avec un read des données + pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController{
     #[route('/', 'home.index', methods: ["GET"] )]
     public function index(): Response {
-        return $this->render('home.html.twig');
+        return $this->render('pages/home.html.twig');
 
     }
 }
---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IngredientController extends AbstractController
+{
+    #[Route('/ingredient', name: 'ingredient')]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        /*Cette fonction récupère tout les ingrédients */
+
+        $ingredients = $paginator->paginate(
+            $repository->findAll(), /*$query, query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        return $this->render('pages/ingredient/index.html.twig', [
+            'ingredients' => $ingredients
+        ]);
+    }
+}
